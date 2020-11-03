ec994cee65f7b5109afc2984527592d5e582009b
Alex Dang <alexdang1993374@gmail.com>
2020-11-04 01:49:08 +0700

    changed port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
 app.use(express.static("client"))
-app.listen(3000,() => {
+app.listen(process.env.PORT || 3000,() => {
     console.log("listening at port 3000")
 })
