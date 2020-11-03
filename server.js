98a293ea6657e6486f1b64c6b450ecd2d59b683e
Alex Dang <alexdang1993374@gmail.com>
2020-11-04 01:53:34 +0700

    static

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const app = express()
-app.use(express.static("client"))
+
+app.use(express.static("./index.html"))
+
 app.listen(process.env.PORT || 3000,() => {
     console.log("listening at port 3000")
 })
